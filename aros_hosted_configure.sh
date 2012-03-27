1da453baa0638acf7d1bc3a4b23103aa75a01f12
Dave Webster <dave@dave-webster.com>
2012-03-27 21:53:18 +0100

    Fixed the $LOAD_PATH in Ruby to point to an AROS path not the PREFIX from the building host.  The configure script now locks this to SYS:Development/lib/ruby/1.8

--- aros_hosted_configure.sh
+++ aros_hosted_configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set this variable to point to your AROS hosted system directory
-aros_dir="NULL"
-
+aros_dir="/home/david/Downloads/AROS-20120323-linux-i386-system"
+aros_gcc="/home/david/Downloads/AROS-20120323-source/bin/linux-i386/tools/crosstools/i386-aros-gcc"
 
 if [ $aros_dir = "NULL" ]; then
 	echo "**************************************************"
@@ -16,7 +16,7 @@ else
 	echo "Your AROS system directory is \""$aros_dir\"
         echo "**************************************************"
 
-	CC='i386-aros-gcc -nix' CC_FOR_BUILD=gcc ./configure --target=i386-pc-aros --host=i386-pc-aros --build=i386-pc-linux --prefix=$aros_dir"/Development" --disable-shared LIBS="-lmui"
+	CC=$aros_gcc" -nix" CC_FOR_BUILD=gcc ./configure --target=i386-pc-aros --host=i386-pc-aros --build=i386-pc-linux --prefix=$aros_dir"/Development" --disable-shared LIBS="-lmui"
 
 fi 
 
